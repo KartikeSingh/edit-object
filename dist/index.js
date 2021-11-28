e2860903186b1b0de6c3f5d69faf08d440372714
KrazyDev <67275060+KartikeSingh@users.noreply.github.com>
2021-11-28 13:21:03 +0530

    🔮 Added a magical option

--- dist/index.js
+++ dist/index.js
@@ -1,10 +1,16 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-function editObject(object, key, value) {
+function editObject(object, key, value, force = false) {
     if (!key || typeof (key) !== "string")
         throw new Error("Key should be a non empty string.");
     const keys = key.split(".");
     keys.reduce((a, b, i) => {
+        if (typeof (a[b]) !== 'object') {
+            if (force)
+                a[b] = {};
+            else
+                throw new Error("Invalid key was provided, provide a valid key or pass force option as true");
+        }
         if (i === keys.length - 1)
             a[b] = value;
         else
--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,14 @@
-export default function editObject(object: indexable, key: string, value: any): Object {
+export default function editObject(object: indexable, key: string, value: any, force = false): Object {
     if (!key || typeof (key) !== "string") throw new Error("Key should be a non empty string.");
 
     const keys: string[] = key.split(".");
+
     keys.reduce((a, b, i) => {
+        if (typeof (a[b]) !== 'object') {
+            if (force) a[b] = {};
+            else throw new Error("Invalid key was provided, provide a valid key or pass force option as true")
+        }
+
         if (i === keys.length - 1)
             a[b] = value;
         else
